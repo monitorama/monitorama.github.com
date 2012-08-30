9f8090fe11e45822b70445d386ba081011fe8313
obfuscurity <jdixon@heroku.com>
2012-08-30 09:53:20 -0400

    show section on nav click

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('This would be the main JS file.');
+
+$('header ul li').on('click', 'a', function() {
+  var sid = ($(this).attr('id'));
+  //console.log(section);
+  $('section').css('visibility', 'hidden').css('height', '0');
+  $('section#' + sid).css('visibility', 'visible').css('height', 'auto');
+  return false;
+});
