2b39b5f052155f59a616fd7aaa33934891c58eae
obfuscurity <jdixon@heroku.com>
2012-08-30 13:18:42 -0400

    avoid id conflicts, passes w3c validator

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,11 +2,11 @@
 $('header ul li').on('click', 'a', function() {
   var sid = ($(this).attr('id'));
   $('section').css('visibility', 'hidden').css('height', '0');
-  $('section#' + sid).css('visibility', 'visible').css('height', 'auto');
+  $('section.' + sid).css('visibility', 'visible').css('height', 'auto');
   return false;
 });
 
 for (var i in speakers) {
-  //$('section#speakers ul').append('<li class="speaker"><img src="http://www.gravatar.com/avatar/' + speakers[i].hash + '" />' + speakers[i].name + '</li>');
-  $('section#speakers ul').append('<li class="speaker">' + speakers[i].name + '</li>');
+  //$('section.speakers ul').append('<li class="speaker"><img src="http://www.gravatar.com/avatar/' + speakers[i].hash + '" />' + speakers[i].name + '</li>');
+  $('section.speakers ul').append('<li class="speaker">' + speakers[i].name + '</li>');
 }
