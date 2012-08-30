56f39dfbc10e06bdfe192d66cc69b579f0acf1f3
obfuscurity <jdixon@heroku.com>
2012-08-30 10:48:58 -0400

    recreate speaker list from json

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,8 +2,13 @@ console.log('This would be the main JS file.');
 
 $('header ul li').on('click', 'a', function() {
   var sid = ($(this).attr('id'));
-  //console.log(section);
   $('section').css('visibility', 'hidden').css('height', '0');
   $('section#' + sid).css('visibility', 'visible').css('height', 'auto');
   return false;
 });
+
+for (var i in speakers) {
+  //console.log(speakers[i].name);
+  //$('section#speakers ul').append('<li class="speaker"><img src="http://www.gravatar.com/avatar/' + speakers[i].hash + '" />' + speakers[i].name + '</li>');
+  $('section#speakers ul').append('<li class="speaker">' + speakers[i].name + '</li>');
+}
