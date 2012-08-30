808867e041aa6581c2509e3ec12db82e5e11206b
obfuscurity <jdixon@heroku.com>
2012-08-30 10:50:15 -0400

    speaker data

---
+++ javascripts/speakers.js
@@ -0,0 +1,72 @@
+var speakers = [
+  {
+    "name": "John Allspaw",
+    "twitter": "jallspaw",
+    "hash": "d203c4b40e3a19c1213a9b9994b11556"
+  },
+  {
+    "name": "Pete Fritchman",
+    "twitter": "fetep",
+    "hash": "fd23fa0562b26f4157a2d7f80095bb07"
+  },
+  {
+    "name": "Jesse Newland",
+    "twitter": "jnewland",
+    "hash": "f317439da90c3176adc8938bcf5181ff"
+  },
+  {
+    "name": "Michael Leinartas",
+    "twitter": "mleinart",
+    "hash": "5cc574d2cc9fdc74747622f668f34ae1"
+  },
+  {
+    "name": "Mathias Meyer",
+    "twitter": "roidrage",
+    "hash": "4d9dd9bd8d3d4d0ba8af2acc41d14006"
+  },
+  {
+    "name": "Aaron Quint",
+    "twitter": "aq",
+    "hash": "f04bfa14141dca6713f0d9caa763e26b"
+  },
+  {
+    "name": "Kyle Kingsbury",
+    "twitter": "aphyr",
+    "hash": ""
+  },
+  {
+    "name": "Sean Porter",
+    "twitter": "portertech",
+    "hash": "98f9dfc2e5e1318ac78b8c716582cd30"
+  },
+  {
+    "name": "Cliff Moon",
+    "twitter": "moonpolysoft",
+    "hash": ""
+  },
+  {
+    "name": "Mark Imbriaco",
+    "twitter": "markimbriaco",
+    "hash": "5cdc13ebb94012de13314f8e3bcfec8b"
+  },
+  {
+    "name": "John E. Vincent",
+    "twitter": "lusis",
+    "hash": "65110d602e44e6d15d917d499a70bda8"
+  },
+  {
+    "name": "Jordan Sissel",
+    "twitter": "jordansissel",
+    "hash": "75fdd336ae5f818276f1cf79e0468b2d"
+  },
+  {
+    "name": "Daniel Schauenberg",
+    "twitter": "mrtazz",
+    "hash": "89e0ad1229121f46047977ac547bd7b4"
+  },
+  {
+    "name": "Joseph Ruscio",
+    "twitter": "josephruscio",
+    "hash": "c098fac08c5445f01e2e5f00ddc5186d"
+  },
+]
