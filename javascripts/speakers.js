ddf7e9f3184ef32d579830a14c80d92397015bac
obfuscurity <jdixon@heroku.com>
2012-08-31 00:49:20 -0400

    add some bios, order by last name

--- javascripts/speakers.js
+++ javascripts/speakers.js
@@ -1,72 +1,86 @@
 var speakers = [
   {
     "name": "John Allspaw",
-    "twitter": "jallspaw",
+    "twitter": "allspaw",
+    "bio": "John has worked in systems operations for over fourteen years in biotech, government and online media. He started out tuning parallel clusters running vehicle crash simulations for the U.S. government, and then moved on to the Internet in 1997. He built the backing infrastructures at Salon, InfoWorld, Friendster, and Flickr. He is now SVP of Tech Operations at Etsy, and is the author of <em>The Art of Capacity Planning</em> and <em>Web Operations</em> published by O'Reilly.",
     "hash": "d203c4b40e3a19c1213a9b9994b11556"
   },
   {
     "name": "Pete Fritchman",
     "twitter": "fetep",
+    "bio": "Pete Fritchman is an Operations Engineer for Mozilla, where one of his primary focuses is working on open source monitoring tools and enhancing application monitoring.",
     "hash": "fd23fa0562b26f4157a2d7f80095bb07"
   },
   {
-    "name": "Jesse Newland",
-    "twitter": "jnewland",
-    "hash": "f317439da90c3176adc8938bcf5181ff"
+    "name": "Mark Imbriaco",
+    "twitter": "markimbriaco",
+    "bio": "Mark Imbriaco is the VP of Technical Operations at LivingSocial, where he obsesses compulsively about organizational and systems resilience of the infrastructure supporting their massively popular local commerce sites. He has been involved in web operations and development since the mid-nineties, working for companies like America Online, 37signals, and Heroku.",
+    "hash": "5cdc13ebb94012de13314f8e3bcfec8b"
+  },
+  {
+    "name": "Kyle Kingsbury",
+    "twitter": "aphyr",
+    "bio": "",
+    "hash": ""
   },
   {
     "name": "Michael Leinartas",
     "twitter": "mleinart",
+    "bio": "",
     "hash": "5cc574d2cc9fdc74747622f668f34ae1"
   },
   {
     "name": "Mathias Meyer",
     "twitter": "roidrage",
+    "bio": "",
     "hash": "4d9dd9bd8d3d4d0ba8af2acc41d14006"
-  },
-  {
-    "name": "Aaron Quint",
-    "twitter": "aq",
-    "hash": "f04bfa14141dca6713f0d9caa763e26b"
-  },
-  {
-    "name": "Kyle Kingsbury",
-    "twitter": "aphyr",
-    "hash": ""
-  },
-  {
-    "name": "Sean Porter",
-    "twitter": "portertech",
-    "hash": "98f9dfc2e5e1318ac78b8c716582cd30"
   },
   {
     "name": "Cliff Moon",
     "twitter": "moonpolysoft",
+    "bio": "",
     "hash": ""
   },
   {
-    "name": "Mark Imbriaco",
-    "twitter": "markimbriaco",
-    "hash": "5cdc13ebb94012de13314f8e3bcfec8b"
+    "name": "Jesse Newland",
+    "twitter": "jnewland",
+    "bio": "",
+    "hash": "f317439da90c3176adc8938bcf5181ff"
   },
   {
-    "name": "John E. Vincent",
-    "twitter": "lusis",
-    "hash": "65110d602e44e6d15d917d499a70bda8"
+    "name": "Sean Porter",
+    "twitter": "portertech",
+    "bio": "",
+    "hash": "98f9dfc2e5e1318ac78b8c716582cd30"
   },
   {
-    "name": "Jordan Sissel",
-    "twitter": "jordansissel",
-    "hash": "75fdd336ae5f818276f1cf79e0468b2d"
-  },
-  {
-    "name": "Daniel Schauenberg",
-    "twitter": "mrtazz",
-    "hash": "89e0ad1229121f46047977ac547bd7b4"
+    "name": "Aaron Quint",
+    "twitter": "aq",
+    "bio": "",
+    "hash": "f04bfa14141dca6713f0d9caa763e26b"
   },
   {
     "name": "Joseph Ruscio",
     "twitter": "josephruscio",
+    "bio": "Joseph Ruscio is a Co-Founder and CTO at Librato, Inc. He’s responsible for the company’s technical strategy, architecture, and generally hacks on all levels of the product. He’s a Ruby enthusiast and the author of the aggregate and rack-test-rest gems.",
     "hash": "c098fac08c5445f01e2e5f00ddc5186d"
   },
+  {
+    "name": "Daniel Schauenberg",
+    "twitter": "mrtazz",
+    "bio": "",
+    "hash": "89e0ad1229121f46047977ac547bd7b4"
+  },
+  {
+    "name": "Jordan Sissel",
+    "twitter": "jordansissel",
+    "bio": "",
+    "hash": "75fdd336ae5f818276f1cf79e0468b2d"
+  },
+  {
+    "name": "John E. Vincent",
+    "twitter": "lusis",
+    "bio": "John E. Vincent is a 17-year veteran of the IT industry and a frequent contributor to the DevOps discussion. He's done everything from working the help desk to building out data centers. He got officially involved with DevOps in early 2010 and fancies himself a developer in his spare time. John live with his wife and two sons in Roswell, Georgia. His code lives on Github.",
+    "hash": "65110d602e44e6d15d917d499a70bda8"
+  }
 ]
