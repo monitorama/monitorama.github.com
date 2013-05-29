f72ff49d5a18735e48c1e12d0e9063e5e62501a7
obfuscurity <jason@dixongroup.net>
2013-05-28 23:38:05 -0400

    add workshop videos and switch to arrays

--- javascripts/speakers.js
+++ javascripts/speakers.js
@@ -4,7 +4,7 @@ var speakers = [
     "github": "jallspaw",
     "twitter": "allspaw",
     "bio": "John has worked in systems operations for over fourteen years in biotech, government and online media. He started out tuning parallel clusters running vehicle crash simulations for the U.S. government, and then moved on to the Internet in 1997. He built the backing infrastructures at Salon, InfoWorld, Friendster, and Flickr. He is now SVP of Tech Operations at Etsy, and is the author of <em>The Art of Capacity Planning</em> and <em>Web Operations</em> published by O'Reilly.",
-    "video": "67158676",
+    "videos": ["67158676"],
     "hash": "d203c4b40e3a19c1213a9b9994b11556"
   },
   {
@@ -12,7 +12,7 @@ var speakers = [
     "github": "",
     "twitter": "DrQz",
     "bio": "Neil Gunther, M.Sc., Ph.D. is an internationally recognized computer science researcher specializing in performance analysis and capacity planning. Prior to starting his own company in 1994, Neil worked on the NASA Voyager and Galileo missions, the Xerox PARC Dragon, and the Pyramid/Siemens RM1000. He is a consultant for Fortune 500 companies and their web sites. Neil has authored several books including: <em>Guerrilla Capacity Planning</em> and the 2nd edition of <em>Analyzing Computer System Performance with Perl::PDQ</em>. He also conducts training classes based on these books. In 2008, Dr. Gunther received the prestigious A.A. Michelson Award from the Computer Measurement Group.",
-    "video": "67159604",
+    "videos": ["67159604","67184408"],
     "hash": "67fdb198bf9c74851c23a7f96980c09c"
   },
   {
@@ -20,6 +20,7 @@ var speakers = [
     "github": "mikedewar",
     "twitter": "mikedewar",
     "bio": "Mike Dewar is a data-scientist at Bitly, a New York tech company that makes long URLs shorter. He has a PhD in modelling dynamic systems from data from the University of Sheffield in the UK, and has worked as a Machine Learning post-doc in The University of Edinburgh and Columbia University. He has been drawing graphs regularly since he was in High School, and is starting to get the hang of it.",
+    "videos": ["67184212"],
     "hash": "614f65ff1221a12c6e6b6a1b5496b2c8"
   },
   {
@@ -27,6 +28,7 @@ var speakers = [
     "github": "fetep",
     "twitter": "fetep",
     "bio": "Pete Fritchman is an Operations Engineer for Onlive, where one of his primary focuses is working on all things operations and monitoring. He has previously worked in Operations for Mozilla and is an active contributor to the open source monitoring ecosystem.",
+    "videos": ["67183888"],
     "hash": "fd23fa0562b26f4157a2d7f80095bb07"
   },
   {
@@ -34,7 +36,7 @@ var speakers = [
     "github": "imbriaco",
     "twitter": "markimbriaco",
     "bio": "Mark Imbriaco hacks on ops at GitHub, where he obsesses compulsively about organizational and systems resilience of the infrastructure supporting millions of users and git repositories. He has been involved in web operations and development since the mid-nineties, working for companies like America Online, 37signals, Heroku, and LivingSocial.",
-    "video": "67178303",
+    "videos": ["67178303"],
     "hash": "5cdc13ebb94012de13314f8e3bcfec8b"
   },
   {
@@ -42,7 +44,7 @@ var speakers = [
     "github": "aphyr",
     "twitter": "aphyr",
     "bio": "Kyle Kingsbury gets far too excited about distributed systems, commutative datatypes, and Lisp. He spent the last six months hacking full-time on Riemann, an open-source event-oriented monitoring system in Clojure, and is now starting a new position at Factual, Inc.",
-    "video": "67181466",
+    "videos": ["67181466"],
     "hash": "e145b50faf662e70c066b13c98921900"
   },
   {
@@ -50,6 +52,7 @@ var speakers = [
     "github": "mleinart",
     "twitter": "mleinart",
     "bio": "A tinkerer at heart, Michael Leinartas has spent most of his life taking things apart to see how they work. He currently works for Twitter as an SRE responsible for core internal services and spends his spare time contributing to the Graphite project.",
+    "videos": ["67184116"],
     "hash": "5cc574d2cc9fdc74747622f668f34ae1"
   },
   {
@@ -57,7 +60,7 @@ var speakers = [
     "github": "roidrage",
     "twitter": "roidrage",
     "bio": "Mathias Meyer is the Infrastructure Head Honcho at Travis CI and the author of the Riak Handbook.",
-    "video": "67160106",
+    "videos": ["67160106"],
     "hash": "4d9dd9bd8d3d4d0ba8af2acc41d14006"
   },
   {
@@ -65,7 +68,7 @@ var speakers = [
     "github": "cliffmoon",
     "twitter": "moonpolysoft",
     "bio": "Cliff Moon is Founder and Chief Technical Officer at Boundary. Prior to Boundary, Cliff was a lead engineer for Powerset (natural language search engine acquired by Microsoft) where he was instrumental in the design, implementation, launch, and operation of many of the company’s production services. Cliff is an active contributor to open source projects, developing the first open-source implementation of Amazon Dynamo and originating the Dynamo Framework. Cliff is an active and well-regarded member of the NoSQL, Scala, and Erlang communities.",
-    "video": "67163208",
+    "videos": ["67163208"],
     "hash": "8e5f3f79fcadc20e69058de6cea7f0f7"
   },
   {
@@ -73,7 +76,7 @@ var speakers = [
     "github": "jnewland",
     "twitter": "jnewland",
     "bio": "Jesse Newland does Ops at GitHub, where he's determined to build the most pleasurable on-call experience any engineer has ever seen. He believes that monitoring, in fact, does NOT suck, but rather that incident response and the existing tools for facilitating such response do. Over the the course of his career, he's surfaced from scuba dives in Belize to find a Blackberry full of Nagios alerts, pulled his car over to the shoulder of 285 in Atlanta to kill leaking Mongrels, and just this morning responded to and resolved an incident from bed using custom iPhone applications built at GitHub.",
-    "video": "67178160",
+    "videos": ["67178160"],
     "hash": "f317439da90c3176adc8938bcf5181ff"
   },
   {
@@ -81,6 +84,7 @@ var speakers = [
     "github": "portertech",
     "twitter": "portertech",
     "bio": "Sean is a toolsmith, with a love for composable systems. A practitioner of passion driven development with an appetite for a good post-mortem. Author of Sensu, a monitoring framework, and the caretaker of its community. A tea connoisseur and admirer of fine wine. Works at Heavy Water Operations, helping people automate and monitor their infrastructure.",
+    "videos": ["67184037"],
     "hash": "98f9dfc2e5e1318ac78b8c716582cd30"
   },
   {
@@ -88,7 +92,7 @@ var speakers = [
     "github": "quirkey",
     "twitter": "aq",
     "bio": "Aaron Quint is a born and raised Brooklynite now living in the Bay Area. He is the CTO of Paperless Post, a Rubyist, a JS-er, an avid open-sourcer and an amateur charcutier.",
-    "video": "67178240",
+    "videos": ["67178240"],
     "hash": "f04bfa14141dca6713f0d9caa763e26b"
   },
   {
@@ -96,7 +100,7 @@ var speakers = [
     "github": "josephruscio",
     "twitter": "josephruscio",
     "bio": "Joseph Ruscio is a Co-Founder and the Chief Technology Officer at Librato. He's responsible for the company's technical strategy, product architecture, and hacks on all levels of their vision for the future of monitoring. Joe has fifteen years of experience developing distributed systems in startups, academia, and the telecommunications industry and he holds a Masters in Computer Science from Virginia Tech. In his spare time he enjoys snowboarding and obsessing over the details of brewing both coffee and beer. He loves graphs.",
-    "video": "67181654",
+    "videos": ["67181654"],
     "hash": "c098fac08c5445f01e2e5f00ddc5186d"
   },
   {
@@ -104,6 +108,7 @@ var speakers = [
     "github": "mrtazz",
     "twitter": "mrtazz",
     "bio": "Daniel is a Software Engineer at Etsy's infrastructure and development tools team. Automation, documentation and simplicity are his usual tools for improving the status quo. He previously worked in systems and network admninistration, on connecting chemical plants to IT systems and as an embedded systems networking engineer. Things he thoroughly enjoys when not writing code include coffee, breakfast, tv shows and basketball.",
+    "videos": ["67183450"],
     "hash": "89e0ad1229121f46047977ac547bd7b4"
   },
   {
@@ -111,7 +116,7 @@ var speakers = [
     "github": "jordansissel",
     "twitter": "jordansissel",
     "bio": "Jordan is a hacker who lives under a rock in San Jose. He practices hate-driven development and gives out hugs upon request. When not hacking, he loves doing awesome things as a dad and a husband.",
-    "video": "67181587",
+    "videos": ["67181587"],
     "hash": "75fdd336ae5f818276f1cf79e0468b2d"
   },
   {
@@ -119,7 +124,7 @@ var speakers = [
     "github": "lauraxt",
     "twitter": "lxt",
     "bio": "Laura Thomson is a Senior Engineering Manager at Mozilla, where she leads the Webtools team, responsible for crash reporting, performance monitoring, l10n tools, and other developer tools. She is the co-author of <em>PHP and MySQL Web Development</em> and <em>MySQL tutorial</em>. She is Australian but lives on a tumbledown farm in Maryland with her husband, daughter, and various horses, cats, chickens and a dog.",
-    "video": "67181379",
+    "videos": ["67181379"],
     "hash": "fead61362ab9263d8cc3af52bd74168e"
   },
   {
@@ -127,7 +132,7 @@ var speakers = [
     "github": "lusis",
     "twitter": "lusis",
     "bio": "John E. Vincent is a 17-year veteran of the IT industry and a frequent contributor to the DevOps discussion. He's done everything from working the help desk to building out data centers. He got officially involved with DevOps in early 2010 and fancies himself a developer in his spare time. John live with his wife and two sons in Roswell, Georgia. His code lives on Github. ",
-    "video": "67178061",
+    "videos": ["67178061"],
     "hash": "65110d602e44e6d15d917d499a70bda8"
   }
 ]
