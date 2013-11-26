3782ac72b31482de389b121bb8cba08bdc869481
obfuscurity <jason@dixongroup.net>
2013-11-26 01:17:47 -0500

    initial batch of speakers

---
+++ javascripts/speakers.js
@@ -0,0 +1,132 @@
+var speakers = [
+  {
+    "name": "Adrian Cockcroft - Keynote",
+    "github": "adrianco",
+    "twitter": "adrianco",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "e896a34f14b98f376519b5ed85d1359c"
+  },
+  {
+    "name": "Hilary Mason - Keynote",
+    "github": "hmason",
+    "twitter": "hmason",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "7864ff98e7542780386dd17bc440607c"
+  },
+  {
+    "name": "Dr Neil J. Gunther",
+    "github": "DrQz",
+    "twitter": "DrQz",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "67fdb198bf9c74851c23a7f96980c09c"
+  },
+  {
+    "name": "Baron Schwartz",
+    "github": "xaprb",
+    "twitter": "xaprb",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "977a4ffac5032d87447dac02b9b40620"
+  },
+  {
+    "name": "Selena Deckelmann",
+    "github": "selenamarie",
+    "twitter": "selenamarie",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "4535e53ad45275fa955c5b05684342c5"
+  },
+  {
+    "name": "Dan Rowe",
+    "github": "draco2003",
+    "twitter": "draco2002",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "b5cc90c7a3545123656605fc87e35723"
+  },
+  {
+    "name": "C. Scott Andreas",
+    "github": "cscotta",
+    "twitter": "cscotta",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "8e9fb7a6332bd3f013786261c171b7d5"
+  },
+  {
+    "name": "Ashe Dryden",
+    "github": "ashedryden",
+    "twitter": "ashedryden",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "29ff20a2ac375ff989f3bff70b71b23d"
+  },
+  {
+    "name": "Matt Aimonetti",
+    "github": "mattetti",
+    "twitter": "mattetti",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "c69521d6e22fc0bbd69337ec8b1698df"
+  },
+  {
+    "name": "Scott Sanders",
+    "github": "jssjr",
+    "twitter": "scott_sanders",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "70bd372389add5e121b7a9a929b2d887"
+  },
+  {
+    "name": "Katherine Daniels",
+    "github": "kdaniels",
+    "twitter": "beerops",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "dd0081cd62e25c796f50e1401132b19c"
+  },
+  {
+    "name": "Lindsay Holmwood",
+    "github": "auxesis",
+    "twitter": "auxesis",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "fad1e9ed293fc5b3ec7d4abdffeb636f"
+  },
+  {
+    "name": "Jesse Reynolds",
+    "github": "jessereynolds",
+    "twitter": "jessereynolds",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "35b765fe746fc35a8e005a75f24bd391"
+  }
+]
