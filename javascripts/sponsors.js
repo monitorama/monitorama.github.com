1f08be13d69e99320bcbb44116d7b956e8a934ea
obfuscurity <jason@dixongroup.net>
2014-02-16 15:22:24 -0500

    sponsors data

---
+++ javascripts/sponsors.js
@@ -0,0 +1,77 @@
+var sponsors = [
+  {
+    "name": "librato",
+    "url": "https://metrics.librato.com/",
+    "bio": "<p>Librato is the creator of Metrics, a cloud-based service that helps businesses monitor all aspects of their operation, be it their cloud or datacenter infrastructure, their applications, their business metrics, or even their sensor network. It is a platform for storing, visualizing, correlating and acting on any and all of the time series data they want to monitor. Whether data is collected with open source collection agents, via language bindings, or via simple scripts, with Metrics all monitoring data is brought together in a unified hosted environment, helping users to see trends, detect signs of problems early, minimize their impact through automated actions, and quickly find and fix their root cause. Its ease of use, and stunning graphs and dashboards make it both easier and more fun to safeguard the availability and performance of any applications.</p>"
+  },
+  {
+    "name": "github",
+    "url": "https://github.com/",
+    "bio": "<p>GitHub provides tools to collaboratively develop and host source code. Code review, issue tracking, wikis with ReST, AsciiDoc, and Markdown support -- our goal is to make it easier for you and your team to build great software, whether your team is your co-workers sitting next to you or strangers across the globe. Open source can use GitHub completely for free, making it a great place to discover interesting projects, while businesses can feel safe with our secure private offerings.</p>"
+  },
+  {
+    "name": "newrelic",
+    "url": "http://newrelic.com/",
+    "bio": "<p>New Relic is a software analytics company that makes sense of billions of metrics about millions of applications in real time. Our comprehensive SaaS-based solution provides one powerful interface for web and native mobile applications and consolidates the performance monitoring data for any chosen technology in your environment. Our 70,000 active accounts use our cloud solution every day to optimize more than 200 billion metrics for 3 million applications. When your brand and customer experience depend on the performance of modern software, New Relic provides insight into your overall environment. Learn more at <a href=\"http://newrelic.com/\" target=\"_blank\">newrelic.com</a>.</p>"
+  },
+  {
+    "name": "salesforce",
+    "url": "http://www.salesforce.com/",
+    "bio": ""
+  },
+  {
+    "name": "puppetlabs",
+    "url": "http://puppetlabs.com/",
+    "bio": ""
+  },
+  {
+    "name": "pingdom",
+    "url": "https://www.pingdom.com/",
+    "bio": "<p> Pingdom is a website and performance monitoring company dedicated to making the web faster and more reliable. We do this by creating easy to use tools and services for website owners, regardless of who they are and what size their websites are.</p><p>With Pingdom, customers will instantly be notified of any issues with their website, DNS, email server, or any other infrastructure. All that is needed is a URL – Pingdom will take care of the rest.</p><p>Pingdom has earned the trust of more than 400,000 customers in over 200 countries including Spotify, Instagram, Twitter, Dropbox, Ebay, MailChimp, and more.</p><p>Follow Pingdom on twitter #pingdom or visit <a href=\"https://www.pingdom.com/\" target=\"_blank\">www.pingdom.com</a>.</p>"
+  },
+  {
+    "name": "stackdriver",
+    "url": "http://www.stackdriver.com/",
+    "bio": ""
+  },
+  {
+    "name": "pagerduty",
+    "url": "http://www.pagerduty.com/",
+    "bio": "<p>PagerDuty is &quot;9-1-1 dispatch&quot; for IT, helping operations engineers and devops resolve problems with their IT systems as quickly as possible. PagerDuty aggregates errors from all of your IT monitoring tools and handles the people part of the equation: alerting (via phone, SMS or email), on-call scheduling and automatic escalation of critical incidents.</p>"
+  },
+  {
+    "name": "elasticsearch",
+    "url": "http://www.elasticsearch.com/",
+    "bio": ""
+  },
+  {
+    "name": "dataloopio",
+    "url": "https://www.dataloop.io/",
+    "bio": ""
+  },
+  {
+    "name": "grok",
+    "url": "http://groksolutions.com/",
+    "bio": ""
+  },
+  {
+    "name": "basho",
+    "url": "http://basho.com/",
+    "bio": ""
+  },
+  {
+    "name": "threatstack",
+    "url": "https://www.threatstack.com/",
+    "bio": ""
+  },
+  {
+    "name": "victorops",
+    "url": "http://victorops.com/",
+    "bio": ""
+  },
+  {
+    "name": "influxdb",
+    "url": "http://influxdb.org/",
+    "bio": "<p>InfluxDB is an open source, distributed time series, metrics, and events database. It's designed for analytics workloads for operations, application, user, and business analytics. Written in Go, InfluxDB has no external dependencies. That means you can get running without managing a Zookeeper, HBase, or Cassandra deployment. With a SQL-like query language and an HTTP API, InfluxDB is useful even if you're running only a single box. However, it's built to scale out to hundreds of machines as your data expands. With a growing community, libraries in many languages, plugins for StatsD, CollectD, Fluent, Sensu and others, InfluxDB is the central data store for all your metrics, events, and analytics. We offer support, professional services, and managed hosting to get you up and running either in your own environment or in the cloud.</p>"
+  }
+]
