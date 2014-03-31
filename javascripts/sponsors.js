208cd639b75be926f9d82a67341b858ba2d9324f
obfuscurity <jason@dixongroup.net>
2014-03-31 10:56:31 -0400

    add salesforce sponsor

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -23,7 +23,7 @@ var sponsors = [
   {
     "name": "salesforce",
     "url": "http://www.salesforce.com/",
-    "bio": ""
+    "bio": "<p>Salesforce.com is the enterprise cloud computing leader. Our social and mobile cloud technologies—including our flagship sales and CRM applications—help companies connect with customers, partners, and employees in entirely new ways.</p>"
   },
   {
     "name": "puppetlabs",
