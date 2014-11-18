937f92047e4c0ccfc61d33490f460a9e7fbdf2cc
obfuscurity <jason@dixongroup.net>
2014-11-18 12:14:32 -0500

    add librato and dataloop sponsors

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -1,12 +1,22 @@
 var sponsors = [
   {
+    "name": "librato",
+    "url": "https://www.librato.com/",
+    "bio": "<p>Librato is the creator of Metrics, a cloud-based service that helps businesses monitor all aspects of their operation, be it their cloud or datacenter infrastructure, their applications, their business metrics, or even their sensor network. It is a platform for storing, visualizing, correlating and acting on any and all of the time series data they want to monitor. Whether data is collected with open source collection agents, via language bindings, or via simple scripts, with Metrics all monitoring data is brought together in a unified hosted environment, helping users to see trends, detect signs of problems early, minimize their impact through automated actions, and quickly find and fix their root cause. Its ease of use, and stunning graphs and dashboards make it both easier and more fun to safeguard the availability and performance of any applications.</p>"
+  },
+  {
+    "name": "dataloopio",
+    "url": "https://www.dataloop.io/",
+    "bio": "<p>Dataloop.IO is a SaaS monitoring application designed for DevOps & Operations teams running online services at scale. We take away the burden of building and maintaining custom monitoring solutions in house, with a modern solution designed specifically for the needs of monitoring cloud services, so our customers can concentrate on deploying, scaling and supporting their products.</p><p>If you're a DevOps team running a Cloud Service at scale, starting to move to micro-services and are pushing monitoring tools out to your development teams, Dataloop.IO has been designed specifically for you so sign up today at <a href=\"http://www.dataloop.io/\">www.dataloop.io</a>.</p>"
+  },
+  /* {
+    "name": "raintank",
+    "url": "http://www.raintank.io/",
+    "bio": "<p></p>"
+  }, */
+  {
     "name": "oreilly",
     "url": "http://www.oreilly.com/",
     "bio": "<p>O'Reilly Media spreads the knowledge of innovators through its books, online services, magazines, research, and conferences. Since 1978, O'Reilly has been a chronicler and catalyst of leading-edge development, homing in on the technology trends that really matter and galvanizing their adoption by amplifying \"faint signals\" from the alpha geeks who are creating the future. An active participant in the technology community, the company has a long history of advocacy, meme-making, and evangelism.</p>"
   }
-  /* {
-    "name": "librato",
-    "url": "https://metrics.librato.com/",
-    "bio": "<p>Librato is the creator of Metrics, a cloud-based service that helps businesses monitor all aspects of their operation, be it their cloud or datacenter infrastructure, their applications, their business metrics, or even their sensor network. It is a platform for storing, visualizing, correlating and acting on any and all of the time series data they want to monitor. Whether data is collected with open source collection agents, via language bindings, or via simple scripts, with Metrics all monitoring data is brought together in a unified hosted environment, helping users to see trends, detect signs of problems early, minimize their impact through automated actions, and quickly find and fix their root cause. Its ease of use, and stunning graphs and dashboards make it both easier and more fun to safeguard the availability and performance of any applications.</p>"
-  }, */
 ]
