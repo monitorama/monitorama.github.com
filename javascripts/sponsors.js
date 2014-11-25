479f0119636f83bf395bc19caa828d9f3fdcf9e0
obfuscurity <jason@dixongroup.net>
2014-11-25 16:07:43 -0500

    add sponsor fastly

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -1,5 +1,11 @@
 var sponsors = [
   {
+    "name": "fastly",
+    "url": "http://www.fastly.com/",
+    "width": 175,
+    "bio": "<p>Fastly is a developer-friendly, Varnish-based CDN that speeds up your site by delivering static, dynamic, and streaming content with the lowest recorded latency. We've built our network for the future with 10Gb Ethernet, multi-core CPUs, and all Solid State Drives (SSDs), and our transparent real-time and historical analytics dashboard provides performance insights.</p><p>Fastly allows you to:</p><ul><li>Cache anything (static & dynamic content, whole pages, APIs & more)</li><li>Control and manage configuration via our API</li><li>Deploy and roll back changes instantly</li><li>Monitor your performance with real-time analytics and logging</li></ul><p>Sign up for a free trial account at <a href=\"http://www.fastly.com/\">http://www.fastly.com</a> and start improving your site performance today!</p>"
+  },
+  {
     "name": "librato",
     "url": "https://www.librato.com/",
     "bio": "<p>Librato is the creator of Metrics, a cloud-based service that helps businesses monitor all aspects of their operation, be it their cloud or datacenter infrastructure, their applications, their business metrics, or even their sensor network. It is a platform for storing, visualizing, correlating and acting on any and all of the time series data they want to monitor. Whether data is collected with open source collection agents, via language bindings, or via simple scripts, with Metrics all monitoring data is brought together in a unified hosted environment, helping users to see trends, detect signs of problems early, minimize their impact through automated actions, and quickly find and fix their root cause. Its ease of use, and stunning graphs and dashboards make it both easier and more fun to safeguard the availability and performance of any applications.</p>"
