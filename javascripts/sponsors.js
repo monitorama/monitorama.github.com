68738fe45fcbe4abb218ddbaeffef9f34693f186
obfuscurity <jason@dixongroup.net>
2014-11-25 11:17:42 -0500

    reorder victorops, they've dropped the donuts/coffee sponsorship

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -1,11 +1,5 @@
 var sponsors = [
   {
-    "name": "victorops",
-    "url": "http://victorops.com/",
-    "width": 350,
-    "bio": "<p>VictorOps makes being on-call suck less.</p><ul><li>On-call schedule management</li><li>Incident push, SMS, email, phone notifications</li><li>One touch on-call handoffs</li><li>Mobile incident ack-ing</li><li>Live infrastructure timeline</li><li>Android and iOS native apps</li><li>Integrations with your existing monitoring tools</li></ul><p><a href=\"http://victorops.com/?signup=1\">Try us free for 30 days</a> - no credit card (or firstborn) required.</p>"
-  },
-  {
     "name": "librato",
     "url": "https://www.librato.com/",
     "bio": "<p>Librato is the creator of Metrics, a cloud-based service that helps businesses monitor all aspects of their operation, be it their cloud or datacenter infrastructure, their applications, their business metrics, or even their sensor network. It is a platform for storing, visualizing, correlating and acting on any and all of the time series data they want to monitor. Whether data is collected with open source collection agents, via language bindings, or via simple scripts, with Metrics all monitoring data is brought together in a unified hosted environment, helping users to see trends, detect signs of problems early, minimize their impact through automated actions, and quickly find and fix their root cause. Its ease of use, and stunning graphs and dashboards make it both easier and more fun to safeguard the availability and performance of any applications.</p>"
@@ -21,11 +15,22 @@ var sponsors = [
     "bio": "<p>Dataloop.IO is a SaaS monitoring application designed for DevOps & Operations teams running online services at scale. We take away the burden of building and maintaining custom monitoring solutions in house, with a modern solution designed specifically for the needs of monitoring cloud services, so our customers can concentrate on deploying, scaling and supporting their products.</p><p>If you're a DevOps team running a Cloud Service at scale, starting to move to micro-services and are pushing monitoring tools out to your development teams, Dataloop.IO has been designed specifically for you so sign up today at <a href=\"http://www.dataloop.io/\">www.dataloop.io</a>.</p>"
   },
   {
+    "name": "victorops",
+    "url": "http://victorops.com/",
+    "width": 350,
+    "bio": "<p>VictorOps makes being on-call suck less.</p><ul><li>On-call schedule management</li><li>Incident push, SMS, email, phone notifications</li><li>One touch on-call handoffs</li><li>Mobile incident ack-ing</li><li>Live infrastructure timeline</li><li>Android and iOS native apps</li><li>Integrations with your existing monitoring tools</li></ul><p><a href=\"http://victorops.com/?signup=1\">Try us free for 30 days</a> - no credit card (or firstborn) required.</p>"
+  },
+  {
     "name": "datadog",
     "url": "http://www.datadoghq.com/",
     "width": 150,
     "bio": "<p>Datadog is a monitoring and analytics platform that covers the full monitoring cycle: from code changes to deployments to alerts. Thanks to its 5-minutes setup, wide application support, pretty charts and open API, Datadog is immediately useful to both development and operations teams.</p>"
   },
+  /* {
+    "name": "hwops",
+    "url": "http://hw-ops.com/",
+    "bio": "<p>We specialize in helping you convert your operations from a cost center to a competitive advantage. We help move operations obstacles out of the way so your development team can build things. The long answer is, well, long.</p>"
+  } */
   {
     "name": "oreilly",
     "url": "http://www.oreilly.com/",
