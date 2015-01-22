7fa1a1ce13185ff283aadf1c9d40be34e629664d
obfuscurity <jason@dixongroup.net>
2015-01-22 10:05:39 -0500

    first batch of speakers, whee

--- javascripts/speakers.js
+++ javascripts/speakers.js
@@ -1,13 +1,122 @@
 var speakers = [
-  /* {
-    "name": "Adrian Cockcroft",
-    "keynote": true,
-    "github": "adrianco",
-    "twitter": "adrianco",
-    "bio": "Adrian Cockcroft has had a long career working at the leading edge of technology. He’s always been fascinated by what comes next, and he writes and speaks extensively on a range of subjects. At Battery, he advises the firm and its portfolio companies about technology issues and also assists with deal sourcing and due diligence.<br /><br />Before joining Battery, Adrian helped lead Netflix’s migration to a large scale, highly available public-cloud architecture and the open sourcing of the cloud-native NetflixOSS platform. Prior to that at Netflix he managed a team working on personalization algorithms and service-oriented refactoring.<br /><br />Adrian was a founding member of eBay Research Labs, developing advanced mobile applications and even building his own homebrew phone, years before iPhone and Android launched. As a distinguished engineer at Sun Microsystems he wrote the best-selling “Sun Performance and Tuning” book and was chief architect for High Performance Technical Computing.<br /><br />He graduated from The City University, London with a Bsc in Applied Physics and Electronics, and was named one of the top leaders in Cloud Computing in 2011 and 2012 by SearchCloudComputing magazine. He can usually be found on Twitter @adrianco.",
-    "abstract": "Please, no More Minutes, Milliseconds, Monoliths... or Monitoring Tools!",
+  {
+    "name": "Inés Sombra",
+    "github": "randommood",
+    "twitter": "randommood",
+    "bio": "Ines Sombra is a Systems Engineer at Fastly, where she spends her time helping the Web go faster. Ines holds an M.S. in Computer Science and an M.S. in Information Management from Washington University in Saint Louis. Being a true Argentine, she has a fondness for steak, fernet, and a pug named Gordo.",
+    "abstract": "",
     "videos": [],
     "slides": [],
-    "hash": "e896a34f14b98f376519b5ed85d1359c"
-  }, */
+    "hash": "0cdf19a89992c05e9e3ec36fbbc3af87"
+  },
+  {
+    "name": "Brendan Gregg",
+    "github": "brendangregg",
+    "twitter": "brendangregg",
+    "bio": "Brendan Gregg is a senior performance architect at Netflix, where he does large scale computer performance design, analysis, and tuning. He is the author of multiple technical books including <em>Systems Performance</em> published by Prentice Hall, and received the USENIX LISA Award for Outstanding Achievement in System Administration. He was previously a performance lead and kernel engineer at Sun Microsystems, where he developed the ZFS L2ARC and worked on performance. He has also created numerous performance analysis tools, which have been included in multiple operating systems. His recent work includes developing methodologies and visualizations for performance analysis.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "12c87172a4fbce038281cd2638dbeb29",
+  },
+  {
+    "name": "Camille Fournier",
+    "github": "skamille",
+    "twitter": "skamille",
+    "bio": "Camille Fournier is the CTO of Rent the Runway, where she and her team of 55 (and growing!) engineers are helping to build the world's first rental platform for luxury goods. Prior to Rent the Runway she was a Vice President at Goldman Sachs, specializing in infrastructure systems. In her limited spare time she contributes to the Apache ZooKeeper project as a committer and PMC member, and helps to oversee the Dropwizard web framework.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "b0050087da18cd0f2efdff3fe637982a"
+  },
+  {
+    "name": "Jeremy Carroll",
+    "github": "phobos182",
+    "twitter": "jeremy_carroll",
+    "bio": "Jeremy is one of the foundational members of the Site Reliability Engineering team at Pinterest. He helps design, build and monitor Pinterest's applications and systems infrastructure that currently handles billions of monthly page views with tremendous growth and scalability challenges. Jeremy has touched nearly every part of the Pinterest production engineering infrastructure. He currently focuses on reliability engineering for back end data and serving infrastructure.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "b36d267cfc5245650585eb82b8b29bdb"
+  },
+  {
+    "name": "Chrissie Brodigan",
+    "github": "chrissiebrodigan",
+    "twitter": "tenaciouscb",
+    "bio": "Chrissie leads user experience research for GitHub. She previously worked at Mozilla and with startups designing experiences to help onboard new users. Long before she was a user researcher, she was a historian who studied and wrote about gender and labor with an emphasis on women’s careers in aviation.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "f091db85bc34be872c938ba9b0901e60"
+  },
+  {
+    "name": "Roy Rapoport",
+    "github": "royrapoport",
+    "twitter": "royrapoport",
+    "bio": "Roy Rapoport manages the Insight Engineering group at Netflix, responsible for building Netflix's Operational Insight platforms, including cloud telemetry, alerting, and real-time analytics. He originally joined Netflix as part of its datacenter-based IT/Ops group, and prior to transferring over to Product Engineering, was managing Service Delivery for IT/Ops.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "419952b0d99c3397799e4dc6a123204f",
+  },
+  {
+    "name": "Laura Thomson",
+    "github": "lauraxt",
+    "twitter": "lxt",
+    "bio": "Laura Thomson is Director of Engineering Operations at Mozilla. Her team is responsible for engineering tools, support and telemetry infrastructures, and the build and release pipelines. Nagios sends her more email than her friends do, and she spends more time looking at graphs than pictures of her family. She has been in various engineering roles at Mozilla for the last seven years, and previously was a Principal at OmniTI, a consultant, and a computer science academic. She has written several books on various Open Source technologies, and is trying to speak at fewer conferences. Laura lives with her family on a rustic horse farm in Maryland, where she relaxes by indulging in manual labor and other rural pursuits.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "fead61362ab9263d8cc3af52bd74168e"
+  },
+  {
+    "name": "Arun Kejariwal",
+    "github": "akejariwal",
+    "twitter": "arun_kejariwal",
+    "bio": "Arun is currently a Capacity and Performance Engineer at Twitter where he works on research and development of novel techniques to improve the accuracy of capacity models and demand forecasts. Prior to joining Twitter, he worked on research and development of practical and statistically rigorous methodologies to deliver high performance, availability and scalability in large scale distributed clusters. Some of the techniques developed have been published in peer-reviewed international conferences/journals. Arun received his Bachelor’s degree in EE from IIT Delhi and doctorate in CS from UCI.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "c861b4498e2702c3546c9e4251b26c38",
+  },
+  {
+    "name": "Danielle Sucher",
+    "github": "DanielleSucher",
+    "twitter": "DanielleSucher",
+    "bio": "Danielle is a software engineer at Stripe, where she works on data infrastructure and machine learning. She loves digging into the guts of things and building art and puzzles and tools and toys, because it’s way easier than sitting still.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "dc9f215c22985887d9929b85b02c49ef"
+  },
+  {
+    "name": "Dan Frank",
+    "github": "danielhfrank",
+    "twitter": "danielhfrank",
+    "bio": "Dan works on the data team at Stripe, and previously worked at Bitly and Trendrr. He spends a lot of time on mountains.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "f23de27bf1eaa7e07152ee18b23f2261"
+  },
+  {
+    "name": "Franklin Hu",
+    "github": "franklinhu",
+    "twitter": "thisisfranklin",
+    "bio": "Franklin works on infrastructure at Stripe. Previously, he has worked on Zipkin–a distributed tracing system–and Observability at Twitter. In his free time, he enjoys board games, Broadway music, and the Oxford comma.",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "37c3b62c818038a84bef21a78a126eec"
+  }
+  /* {
+    "name": "",
+    "github": "",
+    "twitter": "",
+    "bio": "",
+    "abstract": "",
+    "videos": [],
+    "slides": [],
+    "hash": "",
+  } */
 ]
