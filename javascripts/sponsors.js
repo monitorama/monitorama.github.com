b4a45f373045060e390a84d46b7bbf585089b48f
obfuscurity <jason@dixongroup.net>
2015-01-30 01:22:44 -0500

    remove

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -92,11 +92,6 @@ var sponsors = [
     "url": "http://newrelic.com/",
     "bio": "<p>New Relic is a software analytics company that makes sense of billions of metrics about millions of applications in real time. Our comprehensive SaaS-based solution provides one powerful interface for web and native mobile applications and consolidates the performance monitoring data for any chosen technology in your environment. Our 70,000 active accounts use our cloud solution every day to optimize more than 200 billion metrics for 3 million applications. When your brand and customer experience depend on the performance of modern software, New Relic provides insight into your overall environment. Learn more at <a href=\"http://newrelic.com/\" target=\"_blank\">newrelic.com</a>.</p>"
   },
-  /* {
-    "name": "zanthos",
-    "url": "http://www.zanthos.com/",
-    "bio": ""
-  }, */
   {
     "name": "oreilly",
     "url": "http://www.oreilly.com/",
