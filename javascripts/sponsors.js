e58b70041d95bf0560c39425cf136781fe0b5d03
obfuscurity <jason@dixongroup.net>
2016-02-21 14:20:07 -0500

    sponsor info

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -1,4 +1,135 @@
 var sponsors = [
+  {
+    "name": "signalfx",
+    "enabled": true,
+    "url": "https://signalfx.com/",
+    "bio": "<p>SignalFx is an advanced monitoring platform for modern applications that lets you create custom analytics pipelines on metrics data collected from thousands or more sources to create meaningful aggregations -- such as percentiles, moving averages and growth rates -- within seconds of receiving data.</p><p>Built on real world experience running web apps at &gt;100k systems. SignalFx can handle multidimensional metrics at any resolution or scale. Use collectd, StatsD, Dropwizard Metrics, our open source client libraries, our open source proxy or any other instrumentation and collection methods you like.</p>"
+  },
+  {
+    "name": "datadog",
+    "enabled": true,
+    "url": "http://www.datadoghq.com/",
+    "width": 150,
+    "bio": "<p>Datadog is a monitoring and analytics platform for large-scale application infrastructure. Combining metrics from servers, databases, and applications, Datadog delivers sophisticated, actionable alerts, and provides real-time visibility of your entire infrastructure. Datadog includes 120+ vendor-supported, prebuilt integrations and monitors hundreds of thousands of hosts.</p>"
+  },
+  {
+    "name": "bigpanda",
+    "enabled": true,
+    "url": "http://bigpanda.io/",
+    "width": 225,
+    "bio": "<p>BigPanda is the next generation in Intelligent Incident Management for IT, NOC and DevOps teams. BigPanda's data science platform brings order to the alert chaos that plagues IT Operations in complex cloud and on-premise operating environments.  BigPanda aggregates, consolidates, and correlates massive amounts of IT alerts, deployments and communications, thus turning them into actionable insights. For more information, visit: <a href=\"http://www.bigpanda.io\">http://www.bigpanda.io</a>.</p>"
+  },
+  {
+    "name": "netuitive",
+    "enabled": true,
+    "url": "http://www.netuitive.com/",
+    "width": 225,
+    "bio": "<p>Netuitive provides a SaaS-based, adaptive monitoring and analytics solution for cloud infrastructure, applications, and services. By ingesting data from multiple sources, Netuitive learns systems behaviors and applies dynamic policies that reduce the manual effort and human-guesswork other monitoring tools typically require. Netuitive’s patented technology enables development and operations teams to visualize the entire stack, automate performance analysis, detect relevant anomalies, and determine efficient capacity utilization. Sign up for your free trial at <a href=\"http://www.netuitive.com/\">www.netuitive.com</a>.</p>"
+  },
+  {
+    "name": "operable",
+    "enabled": true,
+    "url": "http://operable.io/",
+    "width": 225,
+    "bio": ""
+  },
+  {
+    "name": "librato",
+    "enabled": false,
+    "url": "https://www.librato.com/?utm_campaign=2016-monitorama&utm_content=logo&utm_medium=sponsor-page&utm_source=monitorama",
+    "bio": "<p>Librato is a complete platform for real-time operations analytics that accepts metrics from any source for real-time aggregation and transformation, anomaly detection, alerting, visual analysis and storage. Librato is dedicated to delivering high performance, beautiful and easy to use tools that make managing your operations more fun and efficient. Librato offers turnkey monitoring solutions for a number of IaaS and PaaS platforms, including AWS, Heroku, and AppHarbor. <a href=\"https://www.librato.com/?utm_campaign=2016-monitorama&utm_content=free-trial&utm_medium=sponsor-page&utm_source=monitorama\">Start your free trial</a> and we’ll hand you back beautiful, interactive dashboards within minutes.</p>",
+  },
+  {
+    "name": "raintank",
+    "enabled": true,
+    "url": "http://www.raintank.io/",
+    "bio": "<p>raintank is a new OpenSaaS infrastructure analytics company, currently in stealth. Instead of feeding you metrics and measurements, we educate you with context and clarity on your entire Internet infrastructure stack. The raintank platform is completely open-source and composable, with fully supported deployments available both on-premise and in the cloud.</p><p>Our first product, currently in closed beta, helps you understand your global network and infrastructure performance in new ways. Please follow us on twitter at <a href=\"https://twitter.com/raintanksaas\">@raintanksaas</a> for further updates.</p>"
+  },
+  {
+    "name": "pagerduty",
+    "enabled": true,
+    "url": "http://www.pagerduty.com/",
+    "bio": "<p>PagerDuty’s operations performance platform helps companies increase reliability. By connecting people, systems and data in a single view, PagerDuty delivers visibility and actionable intelligence across global operations for effective incident resolution management. PagerDuty has over 100 platform partners, and is trusted by Fortune 500 companies and startups alike, including Microsoft, National Instruments, Electronic Arts, Adobe, Rackspace, Etsy, Square and Github. The company is headquartered in San Francisco and backed by leading venture capital firms Andreessen Horowitz, Bessemer Venture Partners, Baseline Ventures, Ignition Partners and others. Try PagerDuty for free, or to get more information, visit <a href=\"http://www.pagerduty.com/\">www.pagerduty.com</a>.</p>"
+  },
+  {
+    "name": "serverdensity",
+    "enabled": true,
+    "url": "https://www.serverdensity.com/",
+    "width": 250,
+    "bio": "<p>Server Density is a SaaS tool which helps you run your infrastructure. It syncs with the major cloud providers and and allows you to monitor your websites and servers from a single console, API and mobile app so you can diagnose problems, maintain uptime and maximise performance.</p>"
+  },
+  {
+    "name": "bmc",
+    "enabled": true,
+    "url": "http://www.bmc.com/",
+    "width": 150,
+    "bio": "<p>BMC is a global leader in software solutions that help IT transform traditional businesses into digital enterprises for the ultimate competitive advantage. Our Digital Enterprise Management set of IT solutions is designed to make digital business fast, seamless, and optimized. From mainframe to mobile to cloud and beyond, we pair high-speed digital innovation with robust IT industrialization—allowing our customers to provide intuitive user experiences with optimized performance, cost, compliance, and productivity. BMC solutions serve more than 15,000 customers worldwide including 82 percent of the Fortune 500®. BMC – Bring IT to Life</p>"
+  },
+  {
+    "name": "salesforce",
+    "enabled": false,
+    "url": "http://www.salesforce.com/",
+    "bio": "<p>Salesforce.com is the enterprise cloud computing leader. Our social and mobile cloud technologies—including our flagship sales and CRM applications—help companies connect with customers, partners, and employees in entirely new ways.</p>"
+  },
+  {
+    "name": "catchpoint",
+    "enabled": true,
+    "url": "http://catchpoint.com/",
+    "width": 240,
+    "bio": "<p>Catchpoint is a leading Digital Performance Analytics company that provides unparalleled insight into your customer-critical services to help you consistently deliver an amazing customer experience. Designed for digital business, Catchpoint is the only end-user experience monitoring (EUM) platform that can simultaneously capture, index and analyze object level performance data inline across the most extensive monitor types and node coverage, enabling a smarter and faster way to preempt issues and optimize service delivery. More that 350 customers in over 30 countries trust Catchpoint to strengthen their brand and grow their business.</p>"
+  },
+  {
+    "name": "dyn",
+    "enabled": false,
+    "url": "http://dyn.com/",
+    "width": 175,
+    "bio": "<p>Dyn solutions are at the core of internet performance. Through <strong>traffic management</strong>, <strong>message management</strong> and <strong>performance assurance</strong>, Dyn connects people through the Internet assuring information gets to where it needs to go faster and more reliably than ever before.</p>"
+  },
+  {
+    "name": "nsone",
+    "enabled": true,
+    "url": "https://ns1.com/",
+    "width": 125,
+    "bio": "<p>NS1's intelligent DNS and traffic management platform, with its data driven architecture and unique Filter Chain routing engine, is purpose-built for the most demanding, mission-critical applications on the Internet. NS1's comprehensive platform technology leverages infrastructure, application, and network data to make intelligent routing decisions in real time, ensuring optimal application performance and reliability. NS1's platform is delivered through its world class Managed DNS Network for customers including Imgur, MaxCDN, Collective Media, OneLogin and many more.</p>"
+  },
+  {
+    "name": "sumologic",
+    "enabled": false,
+    "url": "http://www.sumologic.com/",
+    "bio": "<p>Sumo Logic is the next-generation machine data analytics company that leverages Big Data for real-time IT insights.  The company's cloud-based service provides customers with real-time interactive analytics at unprecedented petabyte scale. The Sumo Logic service is powered by patent-pending Elastic Log Processing™ and LogReduce™ technologies, and transforms log data into actionable insights for IT operations, application management, and security and compliance teams. Unlike expensive and complex premise-based solutions, the Sumo Logic service has a low TCO, can be deployed instantly, scales elastically and requires zero maintenance. For more information, visit <a href=\"http://www.sumologic.com/\">www.sumologic.com</a>.</p>"
+  },
+  {
+    "name": "google",
+    "enabled": false,
+    "url": "https://cloud.google.com/",
+    "width": 275,
+    "bio": "<p>Google Cloud Platform enables developers to build, test and deploy applications on Google’s highly-scalable and reliable infrastructure. Choose from computing, storage and application services for your web, mobile and backend solutions.</p>"
+  },
+  {
+    "name": "chef",
+    "enabled": false,
+    "url": "https://www.chef.io/",
+    "width": 150,
+    "bio": "<p>We are Chef – the leader in high-velocity IT automation. We give you a model for automating IT infrastructure and applications that drive self-reliance across your development and operations teams. We are the Chef community. We are tens of thousands strong. We are helping your businesses become faster, safer and more flexible, so you win in today's 24x7 digital economy. Join our movement today.</p>"
+  },
+  {
+    "name": "anodot",
+    "enabled": false,
+    "url": "",
+    "bio": ""
+  },
+  {
+    "name": "sentry",
+    "enabled": false,
+    "url": "",
+    "bio": ""
+  },
+  {
+    "name": "opsclarity",
+    "enabled": false,
+    "url": "",
+    "bio": ""
+  },
   /* {
     "name": "",
     "url": "",
