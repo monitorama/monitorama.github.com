8232decc98d6bd7192388f67891d571f61a7e7a3
obfuscurity <jason@dixongroup.net>
2016-05-25 14:40:37 -0400

    beerops' talk title

--- javascripts/speakers.js
+++ javascripts/speakers.js
@@ -92,7 +92,7 @@ var rawSpeakers = [
     "github": "kdaniels",
     "twitter": "beerops",
     "bio": "Katherine Daniels is a senior operations engineer at Etsy who got her start in programming with TI-80 calculators back when Geocities was still cool, and these days has opinions on things like ‘the cloud’ and ‘devops’. Before escaping to the world of operations, she spent a few years doing R&D and systems engineering in the corporate world. She lives in Brooklyn with a perfectly reasonable number of cats, and in her spare time can often be found rock-climbing or hand crafting knitted server koozies for the data center. She is co-author of O’Reilly Media’s Effective DevOps.",
-    "title": "",
+    "title": "How to Teach an Old Monitoring System New Tricks",
     "abstract": "",
     "videos": [],
     "slides": [],
