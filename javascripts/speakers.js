c7695179bffe920d9b517f704db57ff79e6b90fa
obfuscurity <jason@dixongroup.net>
2016-05-25 14:29:03 -0400

    cheslock's talk title

--- javascripts/speakers.js
+++ javascripts/speakers.js
@@ -81,7 +81,7 @@ var rawSpeakers = [
     "github": "petecheslock",
     "twitter": "petecheslock",
     "bio": "As the head of Threat Stack’s operations and support teams, Pete Cheslock is focused on delivering the highest level of service, reliability, and customer satisfaction to Threat Stack’s growing user base. An industry veteran with over 15 years’ experience in operations, Pete understands the challenges and issues faced by security, development, and operations professionals every day, and how they can help. Prior to Threat Stack, Pete held senior positions at Dyn and Sonian, where he built, managed, and developed automation and release engineering teams and projects.",
-    "title": "",
+    "title": "Everything @obfuscurity taught me about Monitoring",
     "abstract": "",
     "videos": [],
     "slides": [],
