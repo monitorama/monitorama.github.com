3105c40fbe575dea4dc499d29be01bb5583b8c4c
obfuscurity <jason@dixongroup.net>
2016-05-26 13:59:10 -0400

    add sponsor bloomberg

--- javascripts/sponsors.js
+++ javascripts/sponsors.js
@@ -185,9 +185,9 @@ var sponsors = [
   },
   {
     "name": "bloomberg",
-    "enabled": false,
+    "enabled": true,
     "url": "http://www.techatbloomberg.com/",
-    "bio": "<p></p>"
+    "bio": "<p>Bloomberg technology helps drive the world’s financial markets. We provide communications platforms, data, analytics, trading platforms, news and information for the world’s leading financial market participants. We deliver through our unrivaled software, digital platforms, mobile applications and state of the art hardware developed by Bloomberg technologists for Bloomberg customers.</p><p>Our over 3,000 technologists work to define, architect, build and deploy complete systems and solutions that anticipate and fulfill our clients’ needs and market demands. We offer critical enterprise computing solutions for the financial services industry to help organizations deliver, decipher and manage data to meet their organizational needs and growing regulatory requirements. As well, we are known for providing unique biometric security to our customers for over a decade enabling them to access their service securely from a desktop computer, mobile phone or tablet. We have provided communications platforms, true electronic social networking and workflow connectivity in the financial world for over two decades.</p>"
   },
   {
     "name": "walmartlabs",
