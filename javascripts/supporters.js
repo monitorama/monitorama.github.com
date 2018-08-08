028f6c5414255394460f4a66cddc4e9935bb5d2a
obfuscurity <jason@dixongroup.net>
2018-08-08 16:16:46 -0700

    stage sponsors

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -6,4 +6,36 @@ var sponsors = [
     "width": 275,
     "bio": "<p></p>"
   } */
+  {
+    "name": "microsoft",
+    "enabled": false,
+    "url": "https://azure.microsoft.com/en-us/?v=18.27",
+    "width": 250,
+    "bio": "<p>Cloud for all.</p><p>Microsoft Azure believes that all individuals and groups should be empowered with the full freedom and power of the cloud. The cloud should not be available to only an elite few. Azure offers the trust, transparency, and humanity that all companies need to navigate, thrive, and endure in this increasingly cloud-powered world of business.</p>"
+  },
+  {
+    "name": "signalfx",
+    "enabled": false,
+    "url": "https://signalfx.com/",
+    "width": 175,
+    "bio": "<p>SignalFx is a leader in real-time operational intelligence for data-driven DevOps. The service discovers and collects metrics across every component in the cloud, replacing traditional point tools and providing real-time visibility into today’s dynamic environments. Leveraging the massively scalable SignalFx platform, the SaaS service is optimized for container and microservices based architectures and provides powerful visualization, proactive alerting, and collaborative triage capabilities across organizations of all sizes. SignalFx is used by Fortune 500 enterprises across financial services, apparel, industrials, telecommunications, media, and by web-scale players like Yelp, Hubspot, Acquia, and Kayak.</p>",
+  },
+  {
+    "name": "wavefront",
+    "enabled": false,
+    "url": "https://www.wavefront.com/",
+    "bio": "<p>Wavefront™ by VMware® is a SaaS-based metrics monitoring and analytics platform that handles the high-scale requirements of modern cloud-native applications. Wavefront’s speed, scale and flexibility allows DevOps and developer teams instant insight into the performance of their highly-distributed cloud-native services. Wavefront’s analytics, query-driven alerts, interactive visualizations, open API, and integrations, all powered by massively scalable time-series database delivers “the first pane of glass” visibility helping DevOps teams detect performance anomalies while ensuring high availability of key cloud services.</p><p>Developers can self-serve and adapt Wavefront analytics to unique needs of their code while gaining visibility into its production behavior.</p>",
+  },
+  {
+    "name": "newrelic",
+    "enabled": false,
+    "url": "https://newrelic.com/",
+    "bio": "<p>New Relic provides the real-time insights that software-driven businesses need to innovate faster. New Relic’s cloud platform makes every aspect of modern software and infrastructure observable, so companies can find and fix problems faster, build high-performing DevOps teams, and speed up transformation projects. Learn why more than 50% of the Fortune 100 trust New Relic at <a href=\"https://newrelic.com/\">newrelic.com</a>.</p>",
+  },
+  {
+    "name": "lightstep",
+    "enabled": false,
+    "url": "https://lightstep.com/",
+    "bio": "<p>LightStep's mission is to cut through the scale and complexity of today's software to help organizations stay in control of their systems. Its first product, LightStep [x]PM, provides an accurate, detailed snapshot of the entire software system at any point in time, enabling organizations to identify bottlenecks and resolve incidents rapidly.</p>"
+  },
 ]
