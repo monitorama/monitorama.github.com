8bba04689107bd718cca901c1c248b77a9746be2
obfuscurity <jason@dixongroup.net>
2018-09-12 13:57:44 -0400

    stage sponsors

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -38,4 +38,31 @@ var sponsors = [
     "url": "https://lightstep.com/",
     "bio": "<p>LightStep's mission is to cut through the scale and complexity of today's software to help organizations stay in control of their systems. Its first product, LightStep [x]PM, provides an accurate, detailed snapshot of the entire software system at any point in time, enabling organizations to identify bottlenecks and resolve incidents rapidly.</p>"
   },
+  {
+    "name": "datadog",
+    "enabled": false,
+    "url": "http://www.datadoghq.com/",
+    "width": 150,
+    "bio": "<p>Datadog is a monitoring and analytics platform for large-scale application infrastructure. Combining metrics from servers, databases, and applications, Datadog delivers sophisticated, actionable alerts, and provides real-time visibility of your entire infrastructure. Datadog includes 120+ vendor-supported, prebuilt integrations and monitors hundreds of thousands of hosts.</p>"
+  },
+  {
+    "name": "catchpoint",
+    "enabled": false,
+    "url": "http://catchpoint.com/",
+    "width": 240,
+    "bio": "<p>Catchpoint is a leading digital experience intelligence company that provides unparalleled insight into customer-critical services to help businesses consistently deliver amazing digital experiences. Catchpoint is the only performance digital experience monitoring platform that provides integrated synthetic and real user monitoring, comprehensive test types, real-time analytics, and a diverse node network to help you continuously preempt performance issues and optimize service delivery. More than 400 customers in over 30 countries trust Catchpoint to strengthen their brands and grow their businesses.</p>"
+  },
+  {
+    "name": "manageengine",
+    "enabled": false,
+    "url": "https://www.manageengine.co.nl/",
+    "bio": "<p>As the IT management division of Zoho Corporation, ManageEngine prioritizes flexible solutions that work for all businesses, regardless of size or budget.</p><p>ManageEngine crafts comprehensive IT management software with a focus on making your job easier. With our 90+ products and free tools cover everything your IT needs, you can take complete control of your IT infrastructure and services—both on-premises and in the cloud.</p><p>For more information, visit <a href=\"https://www.manageengine.co.nl/\">https://www.manageengine.co.nl/</a></p>"
+  },
+  {
+    "name": "gremlin",
+    "enabled": false,
+    "url": "https://www.gremlin.com/",
+    "width": 150,
+    "bio": "<p>Downtime is expensive. It hurts customer trust. And existing solutions are reactive. Gremlin’s proactive failure testing finds weaknesses in your system before they cause problems. Think of it as vaccinating your network. In a safe, secure, and controlled manner, our software injects failures like packet loss or CPU memory maximization, allowing you to find possible weak spots, resolve them, and make your network stronger. For nearly a decade, we’ve been doing this type of failure testing at companies like Amazon and Netflix. We bring this expertise to your network and harden your systems.</p>"
+  },
 ]
