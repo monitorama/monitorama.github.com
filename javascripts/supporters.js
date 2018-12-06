b68ccb2a103e67365099a5e135b55ddb2925716a
obfuscurity <jason@dixongroup.net>
2018-12-06 15:34:06 -0500

    fix order

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -53,17 +53,17 @@ var sponsors = [
     "bio": "<p>Catchpoint is a leading digital experience intelligence company that provides unparalleled insight into customer-critical services to help businesses consistently deliver amazing digital experiences. Catchpoint is the only performance digital experience monitoring platform that provides integrated synthetic and real user monitoring, comprehensive test types, real-time analytics, and a diverse node network to help you continuously preempt performance issues and optimize service delivery. More than 400 customers in over 30 countries trust Catchpoint to strengthen their brands and grow their businesses.</p>"
   },
   {
-    "name": "manageengine",
-    "enabled": false,
-    "url": "https://www.manageengine.com/",
-    "bio": "<p>As the IT management division of Zoho Corporation, ManageEngine prioritizes flexible solutions that work for all businesses, regardless of size or budget.</p><p>ManageEngine crafts comprehensive IT management software with a focus on making your job easier. With our 90+ products and free tools cover everything your IT needs, you can take complete control of your IT infrastructure and services—both on-premises and in the cloud.</p><p>For more information, visit <a href=\"https://www.manageengine.com/\">https://www.manageengine.co.nl/</a></p>"
-  },
-  {
     "name": "gremlin",
     "enabled": true,
     "url": "https://www.gremlin.com/",
     "width": 150,
     "bio": "<p>Downtime is expensive. It hurts customer trust. And existing solutions are reactive. Gremlin’s proactive failure testing finds weaknesses in your system before they cause problems. Think of it as vaccinating your network. In a safe, secure, and controlled manner, our software injects failures like packet loss or CPU memory maximization, allowing you to find possible weak spots, resolve them, and make your network stronger. For nearly a decade, we’ve been doing this type of failure testing at companies like Amazon and Netflix. We bring this expertise to your network and harden your systems.</p>"
+  },
+  {
+    "name": "manageengine",
+    "enabled": false,
+    "url": "https://www.manageengine.com/",
+    "bio": "<p>As the IT management division of Zoho Corporation, ManageEngine prioritizes flexible solutions that work for all businesses, regardless of size or budget.</p><p>ManageEngine crafts comprehensive IT management software with a focus on making your job easier. With our 90+ products and free tools cover everything your IT needs, you can take complete control of your IT infrastructure and services—both on-premises and in the cloud.</p><p>For more information, visit <a href=\"https://www.manageengine.com/\">https://www.manageengine.co.nl/</a></p>"
   },
   {
     "name": "reactiveops",
