d3ddd39de6a1c8a3c53477b3cce194c9bad4ff50
obfuscurity <jason@dixongroup.net>
2019-01-17 22:31:47 -0500

    add sponsor appdynamics

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -85,6 +85,13 @@ var sponsors = [
     "bio": "<p>BigPanda Inc. enables large enterprises to automate and scale IT Operations to meet the complex demands of digital transformation. The company is the first provider of an Autonomous Digital Operations platform that intelligently automates IT incident management. Customers such as Intel, TiVo, Turner Broadcasting, and Workday rely on BigPanda to minimize operating costs, improve service availability, and reduce IT risk. The BigPanda Platform delivers superior time-to-value by correlating operational data, automating the incident management lifecycle, and streamlining manual processes. Founded in 2012, BigPanda is backed by top-tier investors including Sequoia Capital, Mayfield, and Battery Ventures. Visit <a href=\"https://app.salesforceiq.com/r?target=5b06e39cc9e77c0079c46d85&t=AFwhZf1sDdIV3acZ8wiWIyduU8PDA_qyYpgagHo5gq9sWqCq94tYD5_b7_bQo5TeEveVdRjObdRhQkQfmZTl1cQkgJ2fqBs_R8ZhOk7e707zFz-RWy-zY6B_hsHQn-GT_nTJOLwpQQAa\">www.bigpanda.io</a> for more information.</p>",
   },
   {
+    "name": "appdynamics",
+    "enabled": true,
+    "url": "https://www.appdynamics.com/",
+    "width": 185,
+    "bio": "<p>AppDynamics empowers the enterprise with a solution for real-time application and business performance. Together with Cisco, we’re helping companies make mission critical and strategic decisions that improve customer experience and accelerate business in a multi-cloud world.</p>"
+  },
+  {
     "name": "reactiveops",
     "enabled": true,
     "url": "https://www.reactiveops.com/",
