7fbaca6839e924cd3cd93698b871c63bde573caa
obfuscurity <jason@dixongroup.net>
2019-02-15 08:42:55 -0500

    add sponsor bigpanda

--- javascripts/supporters.js
+++ javascripts/supporters.js
@@ -79,7 +79,7 @@ var sponsors = [
   },
   {
     "name": "bigpanda",
-    "enabled": false,
+    "enabled": true,
     "url": "http://bigpanda.io/",
     "width": 225,
     "bio": "<p>BigPanda Autonomous Operations platform intelligently automates IT incident management. Customers such as Intel, TiVo, Turner Broadcasting, and Workday rely on BigPanda to minimize operating costs, improve service availability, and reduce IT risk. The BigPanda Platform delivers superior time-to-value by correlating operational data, automating the incident management lifecycle, and streamlining manual processes. Founded in 2012, BigPanda is backed by top-tier investors including Sequoia Capital, Mayfield, and Battery Ventures. Visit <a href=\"http://www.bigpanda.io/\">www.bigpanda.io</a> for more information.</p>"
