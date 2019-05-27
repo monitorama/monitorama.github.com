8b5f4c49d01bac5da2a8de1335e6b9a7fbce44a9
obfuscurity <jason@dixongroup.net>
2019-05-27 14:38:53 -0400

    john's talk title

--- 2019/javascripts/speakers.js
+++ 2019/javascripts/speakers.js
@@ -6,7 +6,7 @@ var rawSpeakers = {
       "gitlab": "",
       "twitter": "allspaw",
       "bio": "John Allspaw has worked in software systems engineering and operations for over twenty years in many different environments. John’s publications include the books The Art of Capacity Planning (2009) and Web Operations (2010) as well as the forward to \"The DevOps Handbook.\" His 2009 Velocity talk with Paul Hammond, \"10+ Deploys Per Day: Dev and Ops Cooperation\" helped start the DevOps movement. John served as CTO at Etsy, and holds an MSc in Human Factors and Systems Safety from Lund University.",
-      "title": "",
+      "title": "Taking Human Performance Seriously In Software",
       "abstract": "",
       "video": "",
       "slides": "",
