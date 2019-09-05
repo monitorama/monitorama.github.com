e480eeb32a083b90d499d15c51d3e7e719a56260
obfuscurity <jason@dixongroup.net>
2019-09-05 08:58:11 -0400

    fix riley's bio

--- 2019/javascripts/speakers.js
+++ 2019/javascripts/speakers.js
@@ -307,7 +307,7 @@ var rawSpeakers = {
       "github": "rileyberton",
       "gitlab": "",
       "twitter": "rileyberton",
-      "bio": "Riley is a Principal SRE at Major League Baseball with over 20 years experience in building complex systems. He has worked on time series databases @circonus, real time streaming data @AppNexus, audio and video recognition systems @viggle, and audio/video call logging systems @eventide before finding a home at baseball. In his spare time you can him on the baseball field playing or at the baseball field watching.",
+      "bio": "Riley is a Principal SRE at Major League Baseball with over 20 years experience in building complex systems. He has worked on time series databases @circonus, real time streaming data @AppNexus, audio and video recognition systems @viggle, and audio/video call logging systems @eventide before finding a home at baseball. In his spare time you can find him on the baseball field playing or at the baseball field watching.",
       "title": "What’s Your Service’s Batting Average?",
       "abstract": "",
       "video": "",
