7bb163442fcd42b9cb9e04e58f383723ffcfb72c
obfuscurity <jason@dixongroup.net>
2020-02-21 08:57:00 -0500

    add sponsors influxdata and catchpoint

--- 2020/javascripts/supporters.js
+++ 2020/javascripts/supporters.js
@@ -14,21 +14,22 @@ var sponsors = {
     },
     {
       "name": "influxdata",
-      "enabled": false,
-      "url": "",
-      "bio": ""
+      "enabled": true,
+      "url": "https://www.influxdata.com/",
+      "bio": "<p><a href=\"https://www.influxdata.com/\">InfluxData</a>, the creators of InfluxDB, delivers a modern open source platform built from the ground up for metrics and events (time series data). Whether the data comes from humans, sensors, or machines, InfluxData empowers developers to build next-generation monitoring, analytics, and IoT applications faster, easier, and to scale delivering real business value quickly. Based in San Francisco, InfluxData customers include Autodesk, Cisco, eBay, and SolarCity. Visit <a href=\"https://www.influxdata.com/\">influxdata.com</a>. Twitter: <a href=\"https://twitter.com/influxdb\">@influxdb</a>.</p>",
     },
     {
       "name": "catchpoint",
-      "enabled": false,
-      "url": "",
-      "bio": ""
+      "enabled": true,
+      "url": "http://catchpoint.com/",
+      "width": 240,
+      "bio": "<p>At Catchpoint we believe that every experience matters. Everyone is a customer, and every customer expects a seamless experience with the applications they use. Catchpoint’s Digital Experience Monitoring Platform helps industry leaders ensure that every experience they deliver across the globe is monitored and optimized. The Catchpoint DEM Platform combines synthetic and real user monitoring to provide complete visibility into every layer of the modern application delivery chain, including networks, application code, CDN, DNS, services, APIs, and more. Catchpoint also offers the world’s largest, most geographically-dispersed monitoring network, utilizing over 700 backbone, broadband, cloud and wireless vantage points. Customer Experience Leaders like Google, L'Oréal, Verizon, Oracle, LinkedIn, Honeywell, and Priceline trust Catchpoint to provide real-time intelligence into applications and services to detect, repair, and optimize digital applications faster. See how Catchpoint can reduce your Mean Time to Detect at <a href=\"http://www.catchpoint.com/trial\">www.catchpoint.com/trial</a>.</p>"
     },
     {
       "name": "flowmill",
       "enabled": true,
       "url": "https://www.flowmill.com/",
-      "width": 250,
+      "width": 240,
       "bio": "<p>Flowmill is a cloud observability tool designed to accelerate fault resolution in production and provide a complete picture of distributed applications by continuously monitoring every service to service interaction as well as the cloud infrastructure and 3rd party APIs. With sub-second measurements and an extremely low latency processing pipeline, Flowmill automatically identifies failing components in real time and generates actionable alerts. Flowmill monitoring has negligible overhead, no sampling, no per-service configuration or code changes, and can be deployed across your entire application with minutes.</p>"
     },
     {
