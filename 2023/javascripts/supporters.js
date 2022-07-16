a2e1d6bf965757bfca025030800cf7a83bd74061
obfuscurity <jason@dixongroup.net>
2022-07-15 22:29:40 -0400

    initial changes for pdx2023

---
+++ 2023/javascripts/supporters.js
@@ -0,0 +1,20 @@
+var sponsors = {
+  "pdx": [
+    {
+      "name": "",
+      "enabled": true,
+      "url": "",
+      "width": 185,
+      "bio": "<p></p>"
+    }
+  ],
+  "baltimore": [
+    /* {
+      "name": "bigpanda",
+      "enabled": true,
+      "url": "http://bigpanda.io/",
+      "width": 225,
+      "bio": "<p>BigPanda Autonomous Operations platform intelligently automates IT incident management. Customers such as Intel, TiVo, Turner Broadcasting, and Workday rely on BigPanda to minimize operating costs, improve service availability, and reduce IT risk. The BigPanda Platform delivers superior time-to-value by correlating operational data, automating the incident management lifecycle, and streamlining manual processes. Founded in 2012, BigPanda is backed by top-tier investors including Sequoia Capital, Mayfield, and Battery Ventures. Visit <a href=\"http://www.bigpanda.io/\">www.bigpanda.io</a> for more information.</p>"
+    }, */
+  ]
+}
