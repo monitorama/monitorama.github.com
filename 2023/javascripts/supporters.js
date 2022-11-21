474c3aa5da222144303cd27fbe0fa0321c228b04
obfuscurity <jason@dixongroup.net>
2022-11-21 14:18:39 -0500

    add sponsor squaredup, stage another

--- 2023/javascripts/supporters.js
+++ 2023/javascripts/supporters.js
@@ -1,11 +1,18 @@
 var sponsors = {
   "pdx": [
     {
-      "name": "",
+      "name": "honeycomb",
+      "enabled": false,
+      "url": "https://www.honeycomb.io/",
+      "width": 225,
+      "bio": "<p>Honeycomb provides full-stack observability, designed for high-cardinality data and collaborative problem-solving, enabling engineers to deeply understand and debug production software together. Founded on the experience of debugging problems at the scale of millions of apps serving tens of millions of users, Honeycomb empowers every engineer to instrument and query the behavior of their system.</p>"
+    },
+    {
+      "name": "squaredup",
       "enabled": true,
-      "url": "",
-      "width": 185,
-      "bio": "<p></p>"
+      "url": "https://squaredup.com/",
+      "width": 215,
+      "bio": "<p>SquaredUp helps modern enterprises build, run & optimize complex digital services. We plug into any data, wherever it lives, & surface it in one place to enable faster, better-informed decisions—think of us as mission control for your digital Ops.</p>"
     }
   ],
   "baltimore": [
