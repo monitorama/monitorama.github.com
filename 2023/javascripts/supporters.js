74bb3df4327b8da838b1aee07a779f9fee234c61
obfuscurity <jason@dixongroup.net>
2022-11-29 21:00:19 -0500

    add observeinc, stage other sponsors

--- 2023/javascripts/supporters.js
+++ 2023/javascripts/supporters.js
@@ -1,11 +1,39 @@
 var sponsors = {
   "pdx": [
     {
+      "name": "bigpanda",
+      "enabled": false,
+      "url": "http://bigpanda.io/",
+      "width": 225,
+      "bio": "<p></p>"
+    },
+    {
+      "name": "axiom",
+      "enabled": false,
+      "url": "https://www.axiom.co/",
+      "width": 225,
+      "bio": "<p></p>"
+    },
+    {
+      "name": "chronosphere",
+      "enabled": false,
+      "url": "https://chronosphere.io/",
+      "width": 225,
+      "bio": "<p>Chronosphere is the only cloud native observability platform that puts you back in control by taming rampant data growth and cloud native complexity while accelerating innovation. Teams at organizations of all sizes trust Chronosphere to help them drive competitive advantage and world-class customer experiences while reducing engineering burnout and eliminating lock-in.</p>"
+    },
+    {
       "name": "honeycomb",
       "enabled": false,
       "url": "https://www.honeycomb.io/",
       "width": 225,
       "bio": "<p>Honeycomb provides full-stack observability, designed for high-cardinality data and collaborative problem-solving, enabling engineers to deeply understand and debug production software together. Founded on the experience of debugging problems at the scale of millions of apps serving tens of millions of users, Honeycomb empowers every engineer to instrument and query the behavior of their system.</p>"
+    },
+    {
+      "name": "observeinc",
+      "enabled": true,
+      "url": "https://www.observeinc.com/",
+      "width": 165,
+      "bio": "<p>Observe is a SaaS Observability offering that enables Engineering and DevOps teams to troubleshoot modern distributed applications 10x faster.</p><p>Observe consolidates machine-generated data emitted by applications and turns it into things that are understandable by humans. Instead of searching for breadcrumbs amongst billions of events, engineers can now ask questions about familiar resources such as customers, containers, builds, tickets, and anything else. Observe maintains a graph of relationships between these resources and how they change over time, which is critical to easily provide context when investigating unknown issues. Finally, because of Observe's unique cloud-native architecture, it is priced based on usage making it up to an order of magnitude less expensive.</p>"
     },
     {
       "name": "squaredup",
