679acc245a8e9076bbe0d6853e1e2ed74e1a2e6f
obfuscurity <jason@dixongroup.net>
2023-02-12 23:50:30 -0500

    stage sponsor

--- 2023/javascripts/supporters.js
+++ 2023/javascripts/supporters.js
@@ -8,6 +8,13 @@ var sponsors = {
       "bio": "<p></p>"
     },
     {
+      "name": "newrelic",
+      "enabled": false,
+      "url": "https://newrelic.com/",
+      "width": 200,
+      "bio": "<p>As a leader in observability, New Relic empowers engineers with a data-driven approach to planning, building, deploying, and running great software. New Relic delivers the only unified data platform that empowers engineers with all telemetry—metrics, events, logs, and traces—paired with powerful full-stack analysis tools to help engineers do their best work with data, not opinion. Delivered through the industry’s first usage-based consumption pricing that’s intuitive and predictable, New Relic gives engineers more value for the money by helping improve planning cycle times, change failure rates, release frequency, and mean time to resolution. This helps the world’s leading brands including adidas Runtastic, American Red Cross, Australia Post, Banco Inter, Chegg, GoTo Group, Signify Health, TopGolf, and World Fuel Services improve uptime, reliability, and operational efficiency to deliver exceptional customer experiences that fuel innovation and growth. Empower your engineers with the data they need to measure, improve, and grow your digital business. <a href=\"https://www.newrelic.com/\">www.newrelic.com</a>.</p>"
+    },
+    {
       "name": "axiom",
       "enabled": false,
       "url": "https://www.axiom.co/",
