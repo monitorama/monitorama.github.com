e053f5227a75c8b63fc48dfd1fe21c90b16fab1e
obfuscurity <jason@dixongroup.net>
2024-02-14 21:42:46 -0500

    add sponsor groundcover

--- 2024/javascripts/supporters.js
+++ 2024/javascripts/supporters.js
@@ -1,21 +1,28 @@
 var sponsors = {
-  "pdx": [
+  pdx: [
     {
-      "name": "axiom",
-      "enabled": true,
-      "url": "https://www.axiom.co/",
-      "width": 175,
-      "bio": "<p>100% of your data for every possible need: o11y, security, analytics, and new insights. —— Stop sampling, observe every event.</p>"
+      name: "axiom",
+      enabled: true,
+      url: "https://www.axiom.co/",
+      width: 175,
+      bio: "<p>100% of your data for every possible need: o11y, security, analytics, and new insights. —— Stop sampling, observe every event.</p>",
     },
     {
-      "name": "honeycomb",
-      "enabled": true,
-      "url": "https://www.honeycomb.io/",
-      "width": 225,
-      "bio": "<p>Honeycomb provides full-stack observability, designed for high-cardinality data and collaborative problem-solving, enabling engineers to deeply understand and debug production software together. Founded on the experience of debugging problems at the scale of millions of apps serving tens of millions of users, Honeycomb empowers every engineer to instrument and query the behavior of their system.</p>"
-    }
+      name: "honeycomb",
+      enabled: true,
+      url: "https://www.honeycomb.io/",
+      width: 225,
+      bio: "<p>Honeycomb provides full-stack observability, designed for high-cardinality data and collaborative problem-solving, enabling engineers to deeply understand and debug production software together. Founded on the experience of debugging problems at the scale of millions of apps serving tens of millions of users, Honeycomb empowers every engineer to instrument and query the behavior of their system.</p>",
+    },
+    {
+      name: "groundcover",
+      enabled: true,
+      url: "https://www.groundcover.com/",
+      width: 195,
+      bio: "<p>groundcover is a cloud-native application monitoring solution that reinvents the domain with eBPF. Built for modern production environments, it enables teams to instantly monitor everything they build and run in the cloud without compromising on cost, granularity, or scale. groundcover requires zero code changes and taps into all application logs, metrics, traces and Kubernetes events with one magical command.</p><p>groundcover’s unique in-cloud architecture keeps your data private, secured and under your control without ever leaving your cluster allowing for full cloud-native observability from infrastructure to application, with one solution.</p>",
+    },
   ],
-  "baltimore": [
+  baltimore: [
     /* {
       "name": "bigpanda",
       "enabled": true,
@@ -23,5 +30,5 @@ var sponsors = {
       "width": 225,
       "bio": "<p>BigPanda Autonomous Operations platform intelligently automates IT incident management. Customers such as Intel, TiVo, Turner Broadcasting, and Workday rely on BigPanda to minimize operating costs, improve service availability, and reduce IT risk. The BigPanda Platform delivers superior time-to-value by correlating operational data, automating the incident management lifecycle, and streamlining manual processes. Founded in 2012, BigPanda is backed by top-tier investors including Sequoia Capital, Mayfield, and Battery Ventures. Visit <a href=\"http://www.bigpanda.io/\">www.bigpanda.io</a> for more information.</p>"
     }, */
-  ]
-}
+  ],
+};
