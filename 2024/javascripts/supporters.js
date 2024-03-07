16736cea62f2870bc83212cf54d2e7135b3c53e4
obfuscurity <jason@dixongroup.net>
2024-03-07 15:24:33 -0500

    add sponsor clickhouse, stage others

--- 2024/javascripts/supporters.js
+++ 2024/javascripts/supporters.js
@@ -1,6 +1,20 @@
 var sponsors = {
   pdx: [
     {
+      name: "chronosphere",
+      enabled: false,
+      url: "https://chronosphere.io/",
+      width: 175,
+      bio: "<p></p>",
+    },
+    {
+      name: "clickhouse",
+      enabled: true,
+      url: "https://clickhouse.com/",
+      width: 215,
+      bio: "<p>ClickHouse is a real-time data warehouse and open-source database optimized for analytics, making it the perfect fit for SQL-based observability. When it comes to time series data such as logs, events, and traces, ClickHouse shines in its ability to perform a range of analytical functions over massive volumes of data - leveraging features like high compression rates to ensure robust performance at scale. With a large and growing integrations ecosystem, ClickHouse also makes it easy to leverage observability tools, like OpenTelemetry, that you love. ClickHouse provides cost-efficiency and blazing speed to power observability platforms at any scale.</p>",
+    },
+    {
       name: "axiom",
       enabled: true,
       url: "https://www.axiom.co/",
@@ -20,6 +34,34 @@ var sponsors = {
       url: "https://www.groundcover.com/",
       width: 195,
       bio: "<p>groundcover is a cloud-native application monitoring solution that reinvents the domain with eBPF. Built for modern production environments, it enables teams to instantly monitor everything they build and run in the cloud without compromising on cost, granularity, or scale. groundcover requires zero code changes and taps into all application logs, metrics, traces and Kubernetes events with one magical command.</p><p>groundcover’s unique in-cloud architecture keeps your data private, secured and under your control without ever leaving your cluster allowing for full cloud-native observability from infrastructure to application, with one solution.</p>",
+    },
+    {
+      name: "grafana",
+      enabled: false,
+      url: "",
+      width: 175,
+      bio: "<p></p>",
+    },
+    {
+      name: "elastic",
+      enabled: false,
+      url: "",
+      width: 175,
+      bio: "<p></p>",
+    },
+    {
+      name: "cribl",
+      enabled: false,
+      url: "",
+      width: 175,
+      bio: "<p></p>",
+    },
+    {
+      name: "mezmo",
+      enabled: false,
+      url: "",
+      width: 175,
+      bio: "<p></p>",
     },
   ],
   baltimore: [
